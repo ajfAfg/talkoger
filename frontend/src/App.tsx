b9c73a7883f5b6deb9f0cf64a2fbac55f18963eb
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2022-07-17 01:57:26 +0900

    create Web App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import TextareaAutosize from "react-textarea-autosize";
+import { validate } from "uuid";
 
-function App() {
-  const [count, setCount] = useState(0)
+type Talkog = {
+  userId: string;
+  talk: string;
+  timestamp: Date;
+};
+
+export const App = () => {
+  const [userId, setUserId] = useState("");
+  const [talkogs, setTalkogs] = useState<Talkog[]>([]);
+  const socketRef = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    socketRef.current = new WebSocket(
+      import.meta.env.VITE_WEBSOCKET_SERVER_URL
+    );
+
+    socketRef.current.addEventListener("message", ({ data }) => {
+      const json = JSON.parse(data);
+      const talkog = {
+        userId: json.UserId,
+        talk: json.Talk,
+        timestamp: new Date(parseInt(json.Timestamp) * 1000),
+      };
+      setTalkogs((talkogs) => [talkog, ...talkogs]);
+    });
+
+    return () => {
+      socketRef.current?.close();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (validate(userId)) {
+      socketRef.current?.send(
+        JSON.stringify({
+          action: "fetchTalkogs",
+          UserId: userId,
+        })
+      );
+    }
+    if (userId === "") {
+      setTalkogs((_) => []);
+    }
+  }, [userId]);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <div className="container mx-auto my-20">
+      <TextareaAutosize
+        className={
+          "textarea resize-none flex m-auto text-center p-4 text-5xl" +
+          (userId === "" ? " " + "textarea-primary" : " " + "textarea-ghost")
+        }
+        placeholder="Type your user ID here"
+        onChange={(e) => setUserId(e.target.value)}
+      />
 
-export default App
+      <div className="mx-40 my-28">
+        {talkogs.length === 0 ? (
+          <></>
+        ) : (
+          talkogs
+            .map(({ talk, timestamp }) => (
+              <div className="grid gap-20 grid-cols-4">
+                <div className="col-span-3">
+                  <p className="break-words text-xl p-4">{talk}</p>
+                </div>
+
+                <div className="flex justify-center">
+                  <div className="m-auto text-center">
+                    {timestamp
+                      .toLocaleString()
+                      .split(", ")
+                      .map((str) => (
+                        <p>{str}</p>
+                      ))}
+                  </div>
+                </div>
+              </div>
+            ))
+            .reduce((acc, elem) => (
+              <>
+                {acc} <div className="divider" /> {elem}
+              </>
+            ))
+        )}
+      </div>
+    </div>
+  );
+};
