0b03e0b415a693ebdd0c9ee366ba47aa91cc37cf
ajfAfg <56056962+ajfAfg@users.noreply.github.com>
2022-07-26 00:43:00 +0900

    remove the unneeded file

--- frontend/src/useInterval.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-export const useInterval = (callback: Function, delay?: number) => {
-  useEffect(() => {
-    const interval = setInterval(() => callback(), delay || 0);
-    return () => clearInterval(interval);
-  }, [callback, delay]);
-};
